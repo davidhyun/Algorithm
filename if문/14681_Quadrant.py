e45ac0d9060a48b9601a2582d55a93ba03db4992
HyunhoChoi <hyunh317@naver.com>
2020-09-08 12:44:55 +0900

    initial commit

---
+++ if문/14681_Quadrant.py
@@ -0,0 +1,28 @@
+#x, y = map(int, input().split())
+
+
+x = int(input("x좌표 입력:"))
+y = int(input("y좌표 입력:"))
+
+if x > 0 and y > 0:
+    print("1사분면")
+elif x < 0 and y > 0:
+    print("2사분면")
+elif x < 0 and y < 0:
+    print("3사분면")
+elif x > 0 and y < 0:
+    print("4사분면")
+
+"""
+x = int(input())
+y = int(input())
+
+if x > 0 and y > 0:
+    print(1)
+elif x < 0 and y > 0:
+    print(2)
+elif x < 0 and y < 0:
+    print(3)
+elif x > 0 and y < 0:
+    print(4)
+"""
---
+++ if문/2753_leap year.py
@@ -0,0 +1,19 @@
+print(11/3) #3.6
+print(11//3) #3, 몫
+print(11%3) #2, 나머지
+
+#윤년 X : 1800, 1900, 2100
+#윤년 O : 2012, 2000, 2400
+year = int(input("연도를 입력하시오 : "))
+if year % 4 == 0 and (year % 100 != 0 or year % 400 == 0):
+    print("윤년이다")
+else:
+    print("윤년이 아니다")
+
+"""
+year = int(input())
+if year % 4 == 0 and (year % 100 != 0 or year % 400 == 0):
+    print("1")
+else:
+    print("0")
+"""
