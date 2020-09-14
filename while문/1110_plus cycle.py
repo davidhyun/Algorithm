ca2e886ca2688097fdd81178219e6efa20bacc87
HyunhoChoi <hyunh317@naver.com>
2020-09-14 22:06:41 +0900

    1100_plus cycle

---
+++ while문/1110_plus cycle.py
@@ -0,0 +1,13 @@
+N = int(input())
+check = N
+new_num = 0
+count = 0
+while True:
+    a = N//10
+    b = N%10
+    new_num = 10*b + (a+b)%10
+    count += 1
+    N = new_num
+    if new_num == check:
+        break
+print(count)
