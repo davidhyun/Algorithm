91cb3751d299eb7a0c34d6796bbbd99fa260c6ec
HyunhoChoi <hyunh317@naver.com>
2020-09-23 23:49:46 +0900

    repeat string

--- str/2675_repeat string.py
+++ str/2675_repeat string.py
@@ -9,3 +9,15 @@ for _ in range(T):
     for x in string:
         P = P + (x * int(times))
     print(P)
+
+"""
+map은 seq의 요소하나하나 돌면서 function을 수행해서 맵 객체로 리턴
+map(function, seq)
+l = [(1,2),(2,3),(3,5),(2,9),(1,2)]
+list(map(lambda x: x[0]+x[1], l)) # result = [3,5,8,11,3]
+
+T = int(input())
+for _ in range(T):
+    N, word = input().split()
+    print(*map(lambda x: x * int(N), word), sep = "")
+"""
