5e8bbc25ebdc29d3deed15cb2d1a2973c02f3f55
HyunhoChoi <hyunh317@naver.com>
2020-10-04 13:09:44 +0900

    Valid Parenthesis String

--- 스택(Stack)/9012_Valid Parenthesis String.py
+++ 스택(Stack)/9012_Valid Parenthesis String.py
@@ -21,3 +21,27 @@ for _ in range(T):
         print("YES")
     elif check_cnt > 0:
         print("NO")
+
+"""
+# flag 없이 함수정의로 스택 문제풀이
+
+import sys
+
+def valid_check(ps):
+    check_list = []
+    for x in ps:
+        if x == "(":
+            check_list.append(x)
+        elif x == ")":
+            if not check_list:
+                return False
+            else:
+                check_list.pop()
+
+    return True if not check_list else False
+
+T = int(sys.stdin.readline())
+for _ in range(T):
+    ps = sys.stdin.readline().strip()
+    print("YES" if valid_check(ps) else "NO")
+"""
