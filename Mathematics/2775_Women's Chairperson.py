d18d90822d0f78382ed25e238df08fa8335a30a8
HyunHo Choi <64063767+Hsquare-get@users.noreply.github.com>
2021-03-30 23:49:07 +0900

    Women's Chairperson

---
+++ Mathematics/2775_Women's Chairperson.py
@@ -0,0 +1,13 @@
+T = int(input())
+
+for _ in range(T):
+    floor = int(input()) # 층
+    num = int(input()) # 호
+
+    f0 = [x for x in range(1, num+1)] # 0층 리스트
+    # 층 수 만큼 반복
+    for k in range(floor):
+        for i in range(1, num): # 1 ~ n-1까지
+            f0[i] += f0[i-1] # 층별 각 호실의 사람 수 변경
+
+    print(f0[-1]) # 가장 마지막 수 출력
