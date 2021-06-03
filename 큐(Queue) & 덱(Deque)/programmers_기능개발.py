f239de54563d28001811866dc14ec993849a913d
HyunHo Choi <hyunh317@naver.com>
2021-06-04 00:54:23 +0900

    기능개발

--- 큐(Queue) & 덱(Deque)/programmers_기능개발.py
+++ 큐(Queue) & 덱(Deque)/programmers_기능개발.py
@@ -1,1 +1,34 @@
 from collections import deque
+
+
+def solution(progresses, speeds):
+    pro_deq = deque(progresses)
+    sp_deq = deque(speeds)
+
+    answer = []
+    day = 0
+    cnt = 0
+
+    while pro_deq:
+        if (pro_deq[0] + day * sp_deq[0]) >= 100:
+            pro_deq.popleft()
+            sp_deq.popleft()
+            cnt += 1
+        else:
+            if cnt != 0:
+                answer.append(cnt)
+                cnt = 0  # cnt 초기화
+            day += 1
+
+    answer.append(cnt)
+    return answer
+
+# CASE1
+# progresses = [93,30,55]
+# speeds = [1,30,5]
+
+# CASE2
+# progresses = [95,90,99,99,80,99]
+# speeds = [1,1,1,1,1,1]
+
+# print(solution(progresses, speeds))
