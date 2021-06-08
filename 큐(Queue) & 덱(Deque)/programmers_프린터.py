9b0aa80ebaa4d2a8dd7e74059e3aedab40402b91
HyunHo Choi <hyunh317@naver.com>
2021-06-09 01:13:20 +0900

    priority queue

--- 큐(Queue) & 덱(Deque)/programmers_프린터.py
+++ 큐(Queue) & 덱(Deque)/programmers_프린터.py
@@ -20,6 +20,18 @@ def solution(priorities, location):
                 if x[0] == location:
                     return idx
 
+def solution2(priorities, location):
+    queue = [(i,p) for i,p in enumerate(priorities)]
+    print_order = 0
+    while True:
+        cur = queue.pop(0)
+        if any(cur[1] < q[1] for q in queue):
+            queue.append(cur)
+        else:
+            print_order += 1
+            if cur[0] == location:
+                return print_order
+
 # CASE1
 # priorities = [2,1,3,2]
 # location = 2
@@ -29,3 +41,4 @@ priorities = [1,1,9,1,1,1]
 location = 0
 
 print(solution(priorities, location))
+print(solution2(priorities, location))
