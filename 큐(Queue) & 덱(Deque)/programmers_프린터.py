68d7568f51c1478122e32886fe6f7e308ef2676a
HyunHo Choi <hyunh317@naver.com>
2021-06-09 01:06:30 +0900

    프린터

--- 큐(Queue) & 덱(Deque)/programmers_프린터.py
+++ 큐(Queue) & 덱(Deque)/programmers_프린터.py
@@ -1,2 +1,31 @@
-def solution():
-    pass
+from collections import deque
+
+
+def solution(priorities, location):
+    q = deque([(loc, priority) for (loc, priority) in enumerate(priorities)])
+    result = []
+    flag = False
+    while q:
+        inspection = q.popleft()
+        for compare in q:
+            if inspection[1] < compare[1]:
+                flag = True
+
+        if flag == True:
+            q.append(inspection)
+            flag = False
+        else:
+            result.append(inspection)
+            for idx, x in enumerate(result, 1):
+                if x[0] == location:
+                    return idx
+
+# CASE1
+# priorities = [2,1,3,2]
+# location = 2
+
+# CASE2
+priorities = [1,1,9,1,1,1]
+location = 0
+
+print(solution(priorities, location))
