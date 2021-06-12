3d491eae09310ef27e56c01ba7be1caf4b7d4ad8
HyunHo Choi <hyunh317@naver.com>
2021-06-13 00:13:43 +0900

    Number Of Cases Of Spy Clothes

--- 해시(Hash)/programmers_위장.py
+++ 해시(Hash)/programmers_위장.py
@@ -1,24 +1,28 @@
+def solution(clothes):
+    clothes_dict = {}
+    for value, key in clothes:
+        clothes_dict.setdefault(key, []).append(value)
+
+    answer = 1
+    for key in clothes_dict.keys():
+        # (모자 개수 + 1) * (안경 개수 + 1) * (신발 개수 + 1)
+        answer = answer * (len(clothes_dict[key]) + 1)
+
+    return answer - 1 # 모두 안입는 경우는 없으니 최종 경우의 수에서 -1
+
+
+def solution2(clothes):
+    from collections import Counter
+    each_key_count = Counter([key for _, key in clothes])
+
+    answer = 1
+    for key in each_key_count:
+        # (모자 개수 + 1) * (안경 개수 + 1) * (신발 개수 + 1)
+        answer *= (each_key_count[key] + 1)
+
+    return answer - 1 # 모두 안입는 경우는 없으니 최종 경우의 수에서 -1
+
+
 clothes = [["yellowhat", "headgear"], ["bluesunglasses", "eyewear"], ["green_turban", "headgear"]]
 # clothes = [["crowmask", "face"], ["bluesunglasses", "face"], ["smoky_makeup", "face"]]
-
-from collections import Counter
-
-
-clothes_dict = {}
-for value, key in clothes:
-    clothes_dict.setdefault(key, []).append(value)
-
-print(clothes_dict)
-print([category for _, category in clothes])
-
-print('*' * 20)
-
-
-counter_each_category = Counter([category for _, category in clothes])
-print(counter_each_category)
-all_possible = 1
-
-for key in counter_each_category:
-    all_possible *= (counter_each_category[key] + 1)
-
-print(all_possible - 1)
+print(solution(clothes))
